23ac1a0c1f29c23b3387d394d5f1da482d641bba
TaryneLeach <taryneleach@gmail.com>
2022-07-28 01:11:35 -0400

    changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,15 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    return (
-        <div>
-            <h2>Welcome To Your Budget App!</h2>
-            
-        </div>
-    )
-}
-
+	return (
+		<div>
+			<h2>Welcome To Your Budget App!</h2>
+			<img clasName="home-gif" src='https://c.tenor.com/oZxBz4rQMvcAAAAC/tenor.gif' alt=''></img>
+            <br></br>
+			<Link className="click-link"to='/transactions'>Click to review your monthly transactions!</Link>
+		</div>
+	);
+};
 
 export default Home;
